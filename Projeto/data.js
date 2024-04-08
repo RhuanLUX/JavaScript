8318dc28bc15654cedba075efdd73b0777b2752c
Alberto <betim009@gmail.com>
2024-04-08 13:56:24 -0300

    Checklist project

--- Projeto/data.js
+++ Projeto/data.js
@@ -1,3 +1,45 @@
-const data = [
-    {}
+export const produtos = [
+    {
+        id: 1,
+        nome: 'nome produto A',
+        tipo: 'Indica',
+        quantidade: 7000, // em gramas
+        preco: 10.00,  // por grama
+        imagem: '', // caminho da imagem
+        title: '', // descrição
+    },
+    {
+        id: 2,
+        nome: 'nome produto B',
+        tipo: 'Sativa',
+        quantidade: 7000, // em gramas
+        preco: 15.00,  // por grama
+        imagem: '', // caminho da imagem
+        title: '', // descrição
+    },
+    {
+        id: 3,
+        nome: 'nome produto C',
+        tipo: 'Ambas',
+        quantidade: 7000, // em gramas
+        preco: 20.00,  // por grama
+        imagem: '', // caminho da imagem
+        title: '', // descrição
+    },
+    {
+        id: 4,
+        nome: 'nome produto D',
+        quantidade: 7000, // em gramas
+        preco: 25.00,  // por grama
+        imagem: '', // caminho da imagem
+        title: '', // descrição
+    },
+    {
+        id: 5,
+        nome: 'nome produto E',
+        quantidade: 7000, // em gramas
+        preco: 30.00,  // por grama
+        imagem: '', // caminho da imagem
+        title: '', // descrição
+    },
 ];
---
+++ Projeto/script.js
@@ -0,0 +1,31 @@
+import { produtos } from "./data.js";
+
+// Criar uma função que retorne todos os produtos
+function getAllProducts() {
+    
+}
+
+// Criar uma função que retorne um produto a partir do ID
+function getProductById(id) {
+    
+}
+
+// Criar uma função que retorne um produto a partir do Nome
+function getProductByName(nome) {
+    
+}
+
+// Criar filtro de produto por tipo do produto
+function getProductByTipo(tipo) {
+    
+}
+
+// Criar um filtro por preço
+function filteredProductByPrice() {
+    
+}
+
+// Criar uma função de comparação, um produto ao lado do outro produto
+function compareProduct(params) {
+    
+}
