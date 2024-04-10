4f2253865a2fdd69730e005f51671d947da4a54d
Rhuan Pablo Da Silva <rhuanp@Rhuans-Air.fritz.box>
2024-04-10 19:14:25 +0200

    Feito estrutura de repeticao com Forof

--- Projeto/data.js
+++ Projeto/data.js
@@ -1,47 +1,52 @@
 export const produtos = [
     {
         id: 1,
-        nome: 'nome produto A',
+        nome: 'GMO Auto',
         tipo: 'Indica',
         quantidade: 7000, // em gramas
         preco: 10.00,  // por grama
         imagem: '', // caminho da imagem
         title: '', // descrição
+        thc: '', // THC do item
     },
     {
         id: 2,
-        nome: 'nome produto B',
+        nome: 'Gorilla GMO',
         tipo: 'Sativa',
         quantidade: 7000, // em gramas
         preco: 15.00,  // por grama
         imagem: '', // caminho da imagem
         title: '', // descrição
+        thc: '', // THC do item
     },
     {
         id: 3,
-        nome: 'nome produto C',
+        nome: 'LSD 90%',
         tipo: 'Ambas',
         quantidade: 7000, // em gramas
         preco: 20.00,  // por grama
         imagem: '', // caminho da imagem
         title: '', // descrição
+        thc: '', // THC do item
     },
     {
         id: 4,
-        nome: 'nome produto D',
+        nome: 'THD 45%',
         tipo: 'Indica',
         quantidade: 7000, // em gramas
         preco: 25.00,  // por grama
         imagem: '', // caminho da imagem
         title: '', // descrição
+        thc: '', // THC do item
     },
     {
         id: 5,
-        nome: 'nome produto E',
+        nome: 'GMOF Auto 25% Sativa',
         tipo: 'Indica',
         quantidade: 7000, // em gramas
         preco: 30.00,  // por grama
         imagem: '', // caminho da imagem
         title: '', // descrição
+        thc: '', // THC do item
     },
 ];
--- Projeto/script.js
+++ Projeto/script.js
@@ -2,9 +2,20 @@ import { produtos } from "./data.js";
 
 // Criar uma função que retorne todos os produtos
 function getAllProducts() {
-    
-}
+    console.log ("Nossa Erva",produtos[0].nome, "Custa",produtos[0].preco)
+    console.log ("Nossa Erva",produtos[1].nome, "Custa",produtos[1].preco)
+    console.log ("Nossa Erva",produtos[2].nome, "Custa",produtos[2].preco)
+    console.log ("Nossa Erva",produtos[3].nome, "Custa",produtos[3].preco)
+    console.log ("Nossa Erva",produtos[4].nome, "Custa",produtos[4].preco)
 
+    console.log ()
+// Abaixo - Estrutura de Repição para não ter que escrever o código para retornar todos os dados manualmente. (Comando 'Forof')
+    for (const element of produtos) { 
+        console.log ("Nossa Erva", element.nome, "Custa", element.preco)
+    }
+
+}
+getAllProducts()
 // Criar uma função que retorne um produto a partir do ID
 function getProductById(id) {
     
@@ -36,6 +47,6 @@ function getProductByTHC(THC){
 }
 
 // Criar um filtro Promoção
-function getProductByTHC(THC){
+function getProductBypromocao(THC){
 
 }
